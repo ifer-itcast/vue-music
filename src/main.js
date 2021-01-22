09839514078be859c1a814d303963941c58d5b2d
ifer-itcast <yangkangkang@itcast.cn>
2021-01-22 16:40:51 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { Row, Button } from 'element-ui'
+
+Vue.use(Row)
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
